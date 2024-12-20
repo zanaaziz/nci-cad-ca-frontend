8a1da32da0570b78304bf1036b09518f71070094
Zana Aziz <mail@zanaaziz.com>
2024-12-20 15:44:45 +0000

    deleted pages

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,7 +4,6 @@ class SessionsController < ApplicationController
   end
 
   def create
-    # Send login request to backend
     response = Faraday.post("#{ENV['API_BASE_URL']}/users/sign_in") do |req|
       req.headers["Content-Type"] = "application/json"
       req.body = { user: { email: params[:email], password: params[:password] } }.to_json
@@ -12,7 +11,7 @@ class SessionsController < ApplicationController
 
     if response.status == 200
       user_data = JSON.parse(response.body)
-      session[:jwt_token] = user_data["token"] # Store JWT in session
+      session[:jwt_token] = user_data["token"]
       redirect_to root_path, notice: "Logged in successfully!"
     else
       flash.now[:alert] = "Invalid email or password"
